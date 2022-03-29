f729178c97fe034a5976ee2a287a8ced272a7e0a
Danial Azeem <dazeem.bscs18seecs@seecs.edu.pk>
2022-03-29 20:03:23 +0500

    Created 3 different products for seeding the databse.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Product.create(name: 'Realme 6',
+               description: 'A good gaming phone you need.',
+               price: 200.99)
+
+Product.create(name: 'Asus Laptop',
+               description: 'A good gaming laptop you would like to have.',
+               price: 900.32)
+
+Product.create(name: 'Dodge Demon',
+               description: 'You know what I mean so do not expect me to explain.',
+               price: 190000)               
