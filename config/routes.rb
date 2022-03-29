11698cea2dec2cc745d297e20777911f49aad867
Danial Azeem <dazeem.bscs18seecs@seecs.edu.pk>
2022-03-29 23:36:37 +0500

    set the root for prodcuts#index and created only two resources index and show using the keyword resources.)

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  get 'products', to: 'products#index', as: 'products'
+  root to: 'products#index'
+  resources :products, only: [:index, :show]
+#  get 'products', to: 'products#index', as: 'products'
 # ^Verb  ^URI          ^controller ^action    ^refer: products_path
-  get 'products/:id', to: 'products#show', as: 'product', id: /\d+/
+#  get 'products/:id', to: 'products#show', as: 'product', id: /\d+/
 # ^Verb  ^URI          ^controller ^action    ^refer: product_path
 end
