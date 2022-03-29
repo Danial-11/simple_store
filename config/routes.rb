7cf75e4d5fec3990f4c0a02d192233fc806beef6
Danial Azeem <dazeem.bscs18seecs@seecs.edu.pk>
2022-03-29 22:55:58 +0500

    Contrained id in the show and showing products with ids.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get 'products', to: 'products#index', as: 'products'
 # ^Verb  ^URI          ^controller ^action    ^refer: products_path
-  get 'products/:id', to: 'products#show', as: 'product'
+  get 'products/:id', to: 'products#show', as: 'product', id: /\d+/
 # ^Verb  ^URI          ^controller ^action    ^refer: product_path
 end
